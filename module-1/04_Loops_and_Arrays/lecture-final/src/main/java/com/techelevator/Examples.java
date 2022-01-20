f16b740bb45bcceb00f1614485fbc3a1f9b4bf46
Ben Langhinrichs <benjamin@techelevator.com>
2022-01-20 12:08:23 -0500

    Day 4 exercises, lecture-final, chat.txt, whiteboard, and class notes

---
+++ module-1/04_Loops_and_Arrays/lecture-final/src/main/java/com/techelevator/Examples.java
@@ -0,0 +1,161 @@
+package com.techelevator;
+
+public class Examples {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+
+		System.out.println("*******************************************");
+		System.out.println("**** INCREMENT AND DECREMENT OPERATORS ****");
+		System.out.println("*******************************************");
+		System.out.println();
+
+		/* The ++ operator increments a variable by 1 (i.e. adds one to the
+		 * value of the variable and assigns the result to the variable) */
+		x = 1;
+		System.out.println("x = " + x);
+
+		x += 3;
+		System.out.println("x = " + x);
+
+		x++;
+		System.out.println("x = " + x);
+
+		x--;
+		System.out.println("x = " + x);
+
+		System.out.println("x = " + x++);
+
+		System.out.println("x = " + ++x);
+
+
+
+
+		/* The -- operator decrements a variable by 1 (i.e. subtracts one from the
+		 * value of the variable and assigns the result to the variable) */
+
+		// when the ++ is used as a postfix operator the increment is performed after evalutation
+
+		// when the ++ is used as a prefix operator the increment is performed before evalutation
+
+		System.out.println("****************************************");
+		System.out.println("**** SHORTHAND ASSIGNMENT OPERATORS ****");
+		System.out.println("****************************************");
+		System.out.println();
+
+		/* The "shorthand operators" are ( += -= *= ).  They have the
+		 * effect of applying the arithmetic operator on the variable
+		 * on the left and the expression on the right and then assigning
+		 * the result to the variable on the left.
+		 *
+		 * So, this:
+		 *
+		 *     x = x + 2;
+		 *
+		 * Is equivalent to this:
+		 *
+		 *     x += 2;
+		 */
+
+		// the right hand side of the operator can be an expression
+		x = 1;
+		x += 5;
+		System.out.println("x = " + x);
+
+		x -= 2;
+		System.out.println("x = " + x);
+
+		x *= 3;
+		System.out.println("x = " + x);
+
+		x /= 2;
+		System.out.println("x = " + x);
+
+
+//		System.out.println("*******************");
+//		System.out.println("**** ARRAYS ****");
+//		System.out.println("*******************");
+//		System.out.println();
+
+		int[] myArray = new int[20];
+		myArray[0] = 1;
+		myArray[1] = 5;
+		myArray[19] = 25;
+		System.out.println("First element is " + myArray[0]);
+		System.out.println("Last element is " + myArray[myArray.length - 1]);
+
+
+		System.out.println("*******************");
+		System.out.println("**** FOR LOOPS ****");
+		System.out.println("*******************");
+		System.out.println();
+
+
+		// Print whether each number between 1 and 10 (inclusive) is even or odd
+
+		// if (condition) statement;
+
+		// for (initialization; condition; iterator) statement;
+
+		for (int i = 1; i <= 10; i++) {
+			int d = 4;
+			if (i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd");
+			}
+		}
+
+
+
+		// loops can be nested
+
+		System.out.println("****************");
+		System.out.println("**** ARRAYS with FOR loops ****");
+		System.out.println("****************");
+		System.out.println();
+
+		// the following line declares an array of Strings with 4 elements
+
+		// the following array has length 3 even when no values have been assigned.
+		int[] myArrayOf3 = new int[3];
+		System.out.println("Element has value even before being assigned " + myArrayOf3[2]);
+		myArrayOf3[0] = 5;
+		myArrayOf3[1] = 2;
+		myArrayOf3[2] = 3;
+
+		for (int i = 0; i < myArrayOf3.length; i++) {
+			myArrayOf3[i] = i*2;
+		}
+		System.out.println("array[1] = "+ myArrayOf3[1]);
+
+		//boolean[] myBooleans = new boolean[] {true, true, true, false};
+		boolean[] myBooleans = {true, true, true, false};
+		//boolean[] myBooleans = new boolean[4];
+
+		String[] strings = new String[5];
+		String[] strings2 = new String[] {"a", "b", "c", "d", "e"};
+		String[] strings3 = {"a", "b", "c", "d", "e"};
+
+
+		System.out.println(myBooleans[0]);
+
+//		myNewArray = myMethod(array);
+//		int[] array2 = new int[array.length*2];
+//		array = array2;
+
+
+
+
+		// The same literal syntax would apply to an array of Strings
+
+		/* Arrays are Objects, so they are "passed by reference".  In other words. when assigning
+		 * the value of one array variable to another array variable, we are assigning a reference
+		 * to the same array to both */
+
+
+
+	}
+
+}
