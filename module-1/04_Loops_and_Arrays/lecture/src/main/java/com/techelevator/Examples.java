115cead9aaad3ee3b1e320ad6e1207895a6688d4
Ben Langhinrichs <benjamin@techelevator.com>
2022-01-20 08:38:59 -0500

    Pushing Day 4 lecture and Day 5 tutorial

---
+++ module-1/04_Loops_and_Arrays/lecture/src/main/java/com/techelevator/Examples.java
@@ -0,0 +1,72 @@
+package com.techelevator;
+
+public class Examples {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+
+//		System.out.println("*******************************************");
+//		System.out.println("**** INCREMENT AND DECREMENT OPERATORS ****");
+//		System.out.println("*******************************************");
+//		System.out.println();
+
+		/* The ++ operator increments a variable by 1 (i.e. adds one to the
+		 * value of the variable and assigns the result to the variable) */
+
+		/* The -- operator decrements a variable by 1 (i.e. subtracts one from the
+		 * value of the variable and assigns the result to the variable) */
+
+		// when the ++ is used as a postfix operator the increment is performed after evalutation
+
+		// when the ++ is used as a prefix operator the increment is performed before evalutation
+
+//		System.out.println("****************************************");
+//		System.out.println("**** SHORTHAND ASSIGNMENT OPERATORS ****");
+//		System.out.println("****************************************");
+//		System.out.println();
+
+		/* The "shorthand operators" are ( += -= *= ).  They have the
+		 * effect of applying the arithmetic operator on the variable
+		 * on the left and the expression on the right and then assigning
+		 * the result to the variable on the left.
+		 *
+		 * So, this:
+		 *
+		 *     x = x + 2;
+		 *
+		 * Is equivalent to this:
+		 *
+		 *     x += 2;
+		 */
+
+		// the right hand side of the operator can be an expression
+
+//		System.out.println("*******************");
+//		System.out.println("**** FOR LOOPS ****");
+//		System.out.println("*******************");
+//		System.out.println();
+
+
+		// Print whether each number between 1 and 10 (inclusive) is even or odd
+
+		// loops can be nested
+
+//		System.out.println("****************");
+//		System.out.println("**** ARRAYS ****");
+//		System.out.println("****************");
+//		System.out.println();
+
+		// the following line declares an array of Strings with 4 elements
+
+		// the following array has length 3 even when no values have been assigned.
+
+
+		// The same literal syntax would apply to an array of Strings
+
+		/* Arrays are Objects, so they are "passed by reference".  In other words. when assigning
+		 * the value of one array variable to another array variable, we are assigning a reference
+		 * to the same array to both */
+	}
+
+}
