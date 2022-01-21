830403cb22e21bf9605ec330f4d18fef80ccbf23
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-01-20 22:56:28 -0500

    ALL DONE FOR REAL THIS TIME HW D3

--- module-1/04_Loops_and_Arrays/lecture/src/main/java/com/techelevator/Examples.java
+++ module-1/04_Loops_and_Arrays/lecture/src/main/java/com/techelevator/Examples.java
@@ -49,6 +49,15 @@ public class Examples {
 
 
 		// Print whether each number between 1 and 10 (inclusive) is even or odd
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd");
+			}
+
+		}
+
 
 		// loops can be nested
 
@@ -57,9 +66,34 @@ public class Examples {
 //		System.out.println("****************");
 //		System.out.println();
 
+
+		int [] myArray = new int[20];
+		myArray [0] = 1;
+		myArray [1] = 5;
+		// myArray [20] = 20;
+		System.out.println("Last element is " + myArray[myArray.length-1]);
+
+
+
+
 		// the following line declares an array of Strings with 4 elements
 
 		// the following array has length 3 even when no values have been assigned.
+
+		int [] myArrayOf3 = new int [3];
+		System.out.println("Element has value before being assigned " + myArrayOf3[2]);
+
+		for (int i = 0; i < myArrayOf3.length; i++) {
+			myArrayOf3[i] = i*2;
+		}
+		System.out.println("array[1] = "+ myArrayOf3[1]);
+
+		boolean[] myBooleans = new boolean[10];
+		System.out.println(myBooleans[0]);
+
+		String[] strings = new String[5];
+		String[] strings2 = new Strings [] {"a", "b", "c"};
+		String[] strings3 = new Strings [] {"a", "b", "c"};
 
 
 		// The same literal syntax would apply to an array of Strings
@@ -67,6 +101,9 @@ public class Examples {
 		/* Arrays are Objects, so they are "passed by reference".  In other words. when assigning
 		 * the value of one array variable to another array variable, we are assigning a reference
 		 * to the same array to both */
+
+
+
 	}
 
 }
