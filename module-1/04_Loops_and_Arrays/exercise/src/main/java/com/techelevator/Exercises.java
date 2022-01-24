48cc6aaa0508388407cf515a23dc2e50a5c53227
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-01-23 19:43:56 -0500

    homework loops and arrays done

--- module-1/04_Loops_and_Arrays/exercise/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_and_Arrays/exercise/src/main/java/com/techelevator/Exercises.java
@@ -11,19 +11,16 @@ public class Exercises {
 	 */
 	public boolean firstLast6(int[] nums) {
 
-		int[] nums = {0, 1, 2, 3, 4, 5, 6};
-		return nums[0];
+		if (nums[0] == 6 || nums[nums.length - 1] == 6)
+		{
+
+			return true;
+		}
+
+		return false;
+
+
 	}
-
-	{
-		return nums[7];
-	}
-
-
-
-		return true;
-	}
-
 
 
 	/*
@@ -35,11 +32,16 @@ public class Exercises {
 	 */
 	public boolean sameFirstLast(int[] nums) {
 
-		int [] nums = 
+		if(nums.length == 0) {
+
+			return false;
+
+			} else if(nums[0] == nums[nums.length-1]) {
+
+			return true;
+		}
 		return false;
-	}
-
-
+		}
 
 
 
@@ -51,8 +53,18 @@ public class Exercises {
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+		int myArray [] = {3, 1, 4};
+
+		return myArray;
+
 	}
+
+
+
+
+
+
+
 
 	/*
 	 4. Given 2 arrays of ints, a and b, return true if they have the same first element or they have 
@@ -62,8 +74,16 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
 	public boolean commonEnd(int[] a, int[] b) {
+
+		if (a[a.length - 1] == b[b.length - 1] || a[0] == b[0]) {
+			return true;
+		}
 		return false;
 	}
+
+
+
+
 
 	/*
 	 5. Given an array of ints length 3, return the sum of all the elements.
@@ -72,8 +92,18 @@ public class Exercises {
 	 sum3([7, 0, 0]) → 7
 	 */
 	public int sum3(int[] nums) {
-		return 0;
+
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+
+		return sum;
 	}
+
+
+
+
 
 	/*
 	 6. Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} 
@@ -83,8 +113,18 @@ public class Exercises {
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+
+		int myArray[] = new int [3];
+
+		myArray [0] = nums [1];
+		myArray [1] = nums [2];
+		myArray [2] = nums [0];
+
+		return myArray;
 	}
+
+
+
 
 	/*
 	 7. Given an array of ints length 3, return a new array with the elements in reverse order, so 
@@ -94,8 +134,21 @@ public class Exercises {
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] reverse3(int[] nums) {
-		return new int[] {};
+
+		int myArray[] = new int [3];
+
+	// I don't understand the 'counter' part
+
+		int counter = 2;
+		for (int i = 0; i < nums.length; i++) {
+			myArray[i] += nums[counter];
+			counter -= 1;
+		}
+
+		return myArray;
 	}
+
+
 
 	/*
 	 8. Given an array of ints length 3, figure out which is larger between the first and last elements 
@@ -105,7 +158,19 @@ public class Exercises {
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
 	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+
+		int temp = 0;
+		if(nums[0] > nums[nums.length-1]) {
+			temp = nums[0];
+
+		} else {
+			temp = nums[nums.length-1];
+		}
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = temp;
+		}
+
+		return nums;
 	}
 
 	/*
@@ -116,7 +181,18 @@ public class Exercises {
 	 sum2([1, 1, 1, 1]) → 2
 	 */
 	public int sum2(int[] nums) {
-		return 0;
+
+		if(nums.length == 0) {
+			return 0;
+
+		} else if(nums.length == 1) {
+			return nums [0];
+
+		} else {
+			return nums[0] + nums[1];
+		}
+
+
 	}
 
 	/*
@@ -127,8 +203,19 @@ public class Exercises {
 	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
 	 */
 	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
+
+		int myArray[] = new int[2];
+
+		myArray[0] = a[1];
+		myArray[1] = b[1];
+
+		return myArray;
 	}
+
+
+
+
+
 
 	/*
 	 11. Return the number of even ints in the given array. Note: the % "mod" operator computes the 
@@ -141,6 +228,10 @@ public class Exercises {
 		return 0;
 	}
 
+
+
+
+
 	/*
 	 12. Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 
 	 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do 
@@ -152,19 +243,45 @@ public class Exercises {
 	 sum13([1, 2, 2, 1, 13, 3, 4]) → 10
 	 */
 	public int sum13(int[] nums) {
-		return 0;
+
+		int sum = 0;
+		for (int i = 0; 13 >= nums.length; i++){
+			sum = nums[i];
+		}
+
+		return sum;
+
 	}
 
+
+
+
+
 	/*
+
+
+
+
 	 13. Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
 	 has22([1, 2, 2]) → true
 	 has22([1, 2, 1, 2]) → false
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
+
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] == 2 && nums[i] == 2) {
+				return true;
+
+			}
+		}
+
 		return false;
 	}
-	
+
+
+
+
 	/*
 	 14. Given an array of ints, return true if the array contains no 1's and no 3's.
 	 lucky13([0, 2, 4]) → true
@@ -172,6 +289,9 @@ public class Exercises {
 	 lucky13([1, 2, 4]) → false
 	 */
 	public boolean lucky13(int[] nums) {
+
+
+
 		return false;
 	}
 
@@ -186,3 +306,5 @@ public class Exercises {
 	}
 
 }
+
+
--- module-1/05_Command_Line_Programs/practice/src/main/java/com/techelevator/MakeChange.java
+++ module-1/05_Command_Line_Programs/practice/src/main/java/com/techelevator/MakeChange.java
@@ -12,6 +12,32 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String amount;
+
+	//	while (condition) {
+	//
+	//	}
+		double costOfItems;
+		do  {
+			System.out.print("Please enter the cost of the items: " );
+			amount = scanner.nextLine();
+
+		}   while (costOfItems >] = 0);
+
+		double amountTendered;
+		do {
+
+			double costOfItems = Double.parseDouble(amount);
+
+			System.out.print("Please enter the amount tendered ");
+			amount = scanner.nextLine();
+			double amountTendered = Double.parseDouble(amount);
+
+		}  while (amountTendered < costOfItems);
+
+		double changeReturned = amountTendered - costOfItems;
+		System.out.println("Change returned: "+ changeReturned);
 
 	}
 
