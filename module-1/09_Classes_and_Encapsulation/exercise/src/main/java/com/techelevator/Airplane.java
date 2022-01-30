800f79e3176805f7835561b6e93898776fdc98b5
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-01-30 12:38:11 -0500

    09_Classes_and_Encapsulation DONE

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -2,4 +2,67 @@ package com.techelevator;
 
 public class Airplane {
 
+    public String planeNumber;
+    public int totalFirstClassSeats;
+    public int bookedFirstClassSeats;
+    public int totalCoachSeats;
+    public int bookedCoachSeats;
+
+// constructor
+
+    public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats) {
+        this.planeNumber = planeNumber;
+        this.totalFirstClassSeats = totalFirstClassSeats;
+        this.totalCoachSeats = totalCoachSeats;
+    }
+
+    public String getPlaneNumber() {
+        return planeNumber;
+    }
+
+    public int getTotalFirstClassSeats() {
+        return totalFirstClassSeats;
+    }
+
+
+    public int getBookedFirstClassSeats() {
+        return bookedFirstClassSeats;
+    }
+
+
+    public int getBookedCoachSeats() {
+        return bookedCoachSeats;
+    }
+
+    public int getAvailableFirstClassSeats() {
+        return totalFirstClassSeats - bookedFirstClassSeats;
+    }
+
+    public int getAvailableCoachSeats() {
+        return totalCoachSeats - bookedCoachSeats;
+    }
+
+    public int getTotalCoachSeats() {
+        return totalCoachSeats;
+    }
+
+    // the method...
+
+    public boolean reserveSeats(boolean forFirstClass, int totalNumberOfSeats) {
+        if (forFirstClass && totalNumberOfSeats <= getAvailableFirstClassSeats()) {
+            bookedFirstClassSeats = bookedFirstClassSeats + totalNumberOfSeats;
+            return true;
+        }
+
+        if (!forFirstClass && totalNumberOfSeats <= getAvailableCoachSeats()) {
+            bookedCoachSeats = bookedCoachSeats + totalNumberOfSeats;
+            return true;
+        }
+
+
+
+    return false;
+}
+
+
 }
