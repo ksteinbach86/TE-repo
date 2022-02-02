6118b469a9c20ae7cb0e6a6504d2693d5954b40b
Ben Langhinrichs <benjamin@techelevator.com>
2022-02-02 08:33:41 -0500

    Lecture for Day 13 Managing Inheritance

---
+++ module-1/13_Managing_Inheritance/lecture/src/main/java/com/techelevator/farm/FarmAnimal.java
@@ -0,0 +1,28 @@
+package com.techelevator.farm;
+
+public class FarmAnimal implements Singable {
+	private String name;
+	private String sound;
+
+	public FarmAnimal(String name, String sound) {
+		this.name = name;
+		this.sound = sound;
+	}
+
+	public String getName( ) {
+		return name;
+	}
+	public String getSound( ) {
+		return sound;
+	}
+
+	public String getSound(boolean sleeping) {
+		if (sleeping) {
+			return "";
+		}
+		else {
+			return this.getSound();
+		}
+	}
+
+}
