e8c4d3bf3f2b80c3bf87338fa7b64497922d2783
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-02-09 20:11:28 -0500

    homework 17 done!

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,99 +1,80 @@
 package com.techelevator;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.awt.geom.Path2D;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URI;
+import java.io.*;
+import java.time.DateTimeException;
+import java.util.Date;
 import java.util.Scanner;
 
-public class FindAndReplace<path> {
+
+public class FindAndReplace {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // create the search for a word
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What would you like to search for?");
+        String searchWord = scanner.nextLine();
+
+        System.out.println("What word would you like to replace " + searchWord + " ?");
+        String replacedWord = scanner.nextLine();
+
+        System.out.println("Please enter the file you're looking to access.");
+        String origin = scanner.nextLine();
+        File originFile = new File(origin);
+
+        System.out.println("Where would you like to put your file?");
+        String destination = scanner.nextLine();
+        File destinationFile = new File(destination);
 
 
+        try (Scanner input = new Scanner(originFile);
+            PrintWriter output = new PrintWriter(destinationFile)) {
+            while (input.hasNextLine()) {
+                String lineOfInput = input.nextLine();
+                output.println(lineOfInput.replace(searchWord, replacedWord));
 
+                }
+            }
 
-    public static File main(String[] args) throws IOException {
-
-        // create the search for a word PLUS case-sensitive
-        //the destination file another try/catch here for invalid destination file
-        //the source file (existing) try/catch here if user enters invalid file
-
-        Scanner fileInput = new Scanner(System.in);
-        System.out.println("Please enter the word you wish to search for >>>");
-        String path = fileInput.nextLine();
-        File inputFile = new File(path);
-
-        if (!inputFile.exists()) {
-            System.out.println(path + " does not exist. So sorry!");
-            System.exit(1);
-
-        } else if (!inputFile.isFile()) {
-            System.out.println(path + " is not a file");
-            System.exit(1);
-
+         catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        return inputFile;
+
     }
-
-
-//    File newPath = new File(path);
-//        System.out.println("Let's create a directory.");
-//        System.out.print("Enter the path of the new directory: ");
-//        path = userInput.nextln();
-//        File newDirectory = new File(path);
-//
-//        if (newDirectory.mkdir()) {
-//            System.out.println(newDirectory.getAbsolutePath() + " already exists");
-//        }
-//        else {
-//            if (newDirectory.mkdir()) {
-//                System.out.println("Made it!");
-//            }
-//            else {
-//                System.out.println("Didn't make it");
-//                System.exit(1);
-//            }
-//        }
-
-
-
-//        File inputFile = getInputFileFromUser();
-//    public static File getInputFileFromUser() {
-
-
-
-    private static void caseSensitiveLine(File inputFile, String searchWord) {
-    }
-
-    //create fir the word to replace the search word with
-    public void shouldReplaceSingleWord() throws IOException {
-        String searchStr0 = "Bacon";
-        String searchStr1 = "bacon";
-        String searchStr2 = "spinach";
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
