2e218c0e1b06dce9ca374aaf887c0c65cedbd914
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-02-22 08:37:04 -0500

    half of Design Homework

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/07_remove_actors.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/07_remove_actors.sql
@@ -1,13 +1,11 @@
 -- 7. Remove the actor appearances in "Avengers: Infinity War" (14 rows)
 -- Note: Don't remove the actors themeselves, just make it so it seems no one appeared in the movie.
-START TRANSACTION;
-DELETE FROM movie;
-SELECT COUNT(*)
+
+
+DELETE 
 FROM movie_actor
+WHERE movie_id = 299536;
 
 
---movie_id 299536
-
-ROLLBACK;
 
 
