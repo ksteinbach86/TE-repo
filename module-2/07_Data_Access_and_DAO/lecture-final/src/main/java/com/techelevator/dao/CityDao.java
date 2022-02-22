775c221fe270a408c2262ebfa63606b8963390f9
Ben Langhinrichs <benjamin@techelevator.com>
2022-02-22 12:44:05 -0500

    Lecture-final, chat, class notes, whiteboard, and exercises for 07 Data Access and DAO

---
+++ module-2/07_Data_Access_and_DAO/lecture-final/src/main/java/com/techelevator/dao/CityDao.java
@@ -0,0 +1,20 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.City;
+
+import java.util.List;
+
+public interface CityDao {
+
+    City getCity(long cityId);
+
+    List<City> getCitiesByState(String stateAbbreviation);
+
+    City createCity(City city);
+
+    void updateCity(City city);
+
+    void addOneHundredToCity(long cityID);
+
+    void deleteCity(long cityId);
+}
