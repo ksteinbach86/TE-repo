123c53207ee63cb5ffc2a4337f9eb000a524c79a
Ben Langhinrichs <benjamin@techelevator.com>
2022-03-04 09:06:20 -0500

    Review day shenanigans

---
+++ module-2/15_Review/lecture-final/server/src/main/java/com/techelevator/reservations/dao/JdbcHotelDao.java
@@ -0,0 +1,72 @@
+package com.techelevator.reservations.dao;
+
+import com.techelevator.reservations.model.Address;
+import com.techelevator.reservations.model.Hotel;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JdbcHotelDao implements HotelDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcHotelDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Hotel> list() {
+        List<Hotel> hotels = new ArrayList<>();
+        SqlRowSet results = jdbcTemplate.queryForRowSet("SELECT id, name, address, address2, city, state, zip, stars, rooms_available, cost_per_night FROM state;");
+        while (results.next()) {
+            hotels.add(mapRowToHotel(results));
+        }
+        return hotels;
+    }
+
+    @Override
+    public void create(Hotel hotel) {
+        String sql = "INSERT INTO hotel (name, address, address2, city, state, zip, stars, rooms_available, cost_per_night) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        Address address = hotel.getAddress();
+        jdbcTemplate.update(sql,
+                hotel.getName(), address.getAddress(), address.getAddress2(), address.getCity(), address.getState(), address.getZip(),
+                hotel.getStars(), hotel.getRoomsAvailable(), hotel.getCostPerNight());
+    }
+
+    @Override
+    public Hotel get(int id) {
+        Hotel hotel = null;
+        String sql = "SELECT id, name, address, address2, city, state, zip, stars, rooms_available, cost_per_night FROM state WHERE id = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+        if (results.next()) {
+            hotel = mapRowToHotel(results);
+        }
+        return hotel;
+    }
+
+    private Hotel mapRowToHotel(SqlRowSet rowSet) {
+        Hotel hotel = new Hotel();
+        hotel.setId(rowSet.getInt("id"));
+        hotel.setName(rowSet.getString("name"));
+        Address address = new Address();
+        address.setAddress(rowSet.getString("address"));
+        address.setAddress2(rowSet.getString("address2"));
+        address.setCity(rowSet.getString("city"));
+        address.setState(rowSet.getString("state"));
+        address.setZip(rowSet.getString("zip"));
+        hotel.setAddress(address);
+        hotel.setStars(rowSet.getInt("stars"));
+        hotel.setRoomsAvailable(rowSet.getInt("rooms_available"));
+        hotel.setCostPerNight(rowSet.getDouble("cost_per_night"));
+
+        return hotel;
+
+    }
+
+
+}
