6cd8345ce544658c1192dad175ea522ceeb51f85
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-03-22 07:47:12 -0400

    hw intro to js

--- module-3/06_Intro_to_JavaScript/exercise/exercises.js
+++ module-3/06_Intro_to_JavaScript/exercise/exercises.js
@@ -158,24 +158,23 @@ function cigarParty(cigars, isWeekend) {
 */
 function fizzBuzz() {
 
-for (let i = 1; i <= 101; i++) {
+for (let i = 1; i <= 30; i++) {
 
-    if ((i % 3 === 0) && (i % 5 === 0)) {
-
-    return 'FizzBuzz';
- }
- 	else if (i % 3 === 0) {
-
-      return 'Fizz';
-  }
- 
-  else if (i % 5 === 0) {
-      return 'Buzz';
-   } 
-   else {
-        return(i);
-   } 
+    if (i % 3 == 0 && i % 5 == 0) {
+    	return 'FizzBuzz';
 }
+ 	else if (i % 3 == 0) {
+      	return 'Fizz';
+}
+  	else if (i % 5 == 0) {
+      	return 'Buzz';
+} 
+   	else {
+
+        return(i);
+
+		} 
+	}	
 }
 
 /*
