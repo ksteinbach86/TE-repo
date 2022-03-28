aac499288fb2f948b8d78c52715132016728640b
Ben Langhinrichs <benjamin@techelevator.com>
2022-03-28 12:31:00 -0400

    Pushed exercises and lecture-final (with some tweaks to get hotpink favorites to work) for 11 Intro to Vue

---
+++ module-3/11_Intro_to_Vue_JS/lecture-final/vue-product-reviews/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id='app'>
+    <header>
+      <img src="./assets/logo.png" alt="Vue logo" />
+    </header>
+
+    <product-review></product-review>
+  </div>
+</template>
+
+<script>
+import ProductReview from "./components/ProductReview.vue";
+
+
+export default {
+  name: 'app',
+  components: {
+    ProductReview
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+</style>
