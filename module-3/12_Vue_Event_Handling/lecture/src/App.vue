1945766f4c74cab1ca8c1eac6aa1d94c05fbef61
Ben Langhinrichs <benjamin@techelevator.com>
2022-03-30 08:52:42 -0400

    Lecture for 12 Vue Event Handling

---
+++ module-3/12_Vue_Event_Handling/lecture/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <header>
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </header>
+    <product-review></product-review>
+  </div>
+</template>
+
+<script>
+import ProductReview from "./components/ProductReview.vue";
+
+export default {
+  name: "app",
+  components: {
+    ProductReview
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 60px auto 0 auto;
+}
+</style>
