0cf361a813d04c34fee7b95dfdc88c03fc33df64
Ben Langhinrichs <benjamin@techelevator.com>
2022-03-31 12:14:18 -0400

    Exercises, lecture-final, chat, and scratch (simple Vue app) for 13 Vue Component Communication

---
+++ module-3/13_Vue_Component_Communication/scratch/scratch/src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello">
+    The message passed in will show up below through one way data binding<br />
+    <h1>{{ msg }}</h1>
+
+    <table border="1" width=800>
+      <tr>
+        <table-cell text="Something" />
+        <table-cell text="Something else" />
+        <table-cell text="Other Stuff" />
+      </tr>
+      <tr>
+        <table-cell v-for="c in columns" v-bind:key="c" v-bind:text="c" />
+      </tr>
+            <tr>
+        <td v-for="c in columns" v-bind:key="c">{{ c }}</td>
+      </tr>
+    </table>
+
+
+
+
+  </div>
+</template>
+
+<script>
+import TableCell from './TableCell.vue';
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      columns: 3
+    }
+  },
+  props: ["msg"],       // Props are HTML attributes passed in when we create the component
+  components: {
+    TableCell
+  }
+}
+</script>
+
+
+
+
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
