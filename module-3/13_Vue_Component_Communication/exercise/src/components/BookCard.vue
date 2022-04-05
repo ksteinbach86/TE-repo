5920a7e6ba32a0f60791e293d244cce256834a3b
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-04-05 08:13:24 -0400

    3 of 6 steps passing wish I could have done more

--- module-3/13_Vue_Component_Communication/exercise/src/components/BookCard.vue
+++ module-3/13_Vue_Component_Communication/exercise/src/components/BookCard.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="card">
+    <h2 class="book-title">{{book.title}}</h2>
+    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' 
+    + book.isbn + '-M.jpg'" />
 
-    <!-- Please leave <img> commented out until directed to remove open and close comment tags in the README.
-    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
-    -->
-    
-  </div>
+    <h3 class="book-author">{{book.author}}</h3>
+    <div id="read">
+        <label for="read"> Read</label>
+        <input type="button" id="read" class=".mark-read" v-on:click="counter += 1" />
+
+
+        <!-- <label for="unread"> Unread</label>
+        <input type="unread" id="unread" class=".mark-unread" v-on:click="counter -= 1" /> -->
+    </div>
+
+    <!-- <form v-on:submit.prevent="addNewBook" v-if="showForm === true">
+      <div class="form-element">
+        <label for="title">Name:</label>
+        <input id="title" type="text" v-model="newBook.title-input" />
+      </div>
+      <div class="form-element">
+        <label for="author">Title:</label>
+        <input id="author" type="text" v-model="newBook.author-input" />
+      </div>
+      <div class="form-element">
+        <label for="isbn">Title:</label>
+        <input id="isbn" type="text" v-model="newIsbn.isbn-input" />
+      </div>
+  </div> -->
 </template>
 
 <script>
 export default {
-    name: 'book-card'
+    name: 'book-card',
+    props: ['book'],
+    methods: {
+        readStatus() {
+            return this.$store.commit('READ_STATUS');
+        }
+    }
+    
 }
+
 </script>
 
 <style>
--- module-3/13_Vue_Component_Communication/exercise/src/components/ReadingList.vue
+++ module-3/13_Vue_Component_Communication/exercise/src/components/ReadingList.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="book-container">
-
+    <book-card
+      v-for="book in $store.state.books"
+      v-bind:key="book.id"
+      v-bind:book="book" />
   </div>
 </template>
 
 <script>
+import BookCard from './BookCard.vue';
+
 
 export default {
-    name: 'reading-list'
+    components: { BookCard },
+      name: 'reading-list',
+    
+
 }
 </script>
 
