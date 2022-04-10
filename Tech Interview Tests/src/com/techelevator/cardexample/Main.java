6ba25af77728c5b820df4231064295d6b823797f
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-04-10 13:02:32 -0400

    working on tech tests for employers etactics done started on insight

--- Tech Interview Tests/src/com/techelevator/cardexample/Main.java
+++ Tech Interview Tests/src/com/techelevator/cardexample/Main.java
@@ -1,78 +1,78 @@
 package com.techelevator.cardexample;
-
-
-
+import ArrayList;
+import array;
 import java.util.*;
+
+// create card object with fields for "Suit" and "Rank"
+// populate a List<card>array with all 52 cards
+// print the size of the deck to the console
+// sample output: 52 cards in the deck.
 
 public class DeckOfCards {
 
+    public static void main(String[] args) {
+
+        ArrayList<card> deck = new ArrayList<card>();
+
+        int index = 0;
+
+        String[] suits = { "Diamonds", "Clubs", "Hearts", "Spades" };
+        String[] ranks = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
+
+        //initialize 
+        //randomly shuffle the deck!
+
+        public static Deck toShuffle(Deck deck) {
+
+            for (int i = 0; i < 52; i++) {
+                int shuffled = (int)(Math.random()*(i + 1));
+                Deck[] toShuffle = deck[i];
+                deck[i] = deck[shuffled];
+                deck[shuffled] = deck[toShuffle];
+
+                }
+
+                return deck;
     
-   // create card object with fields for "Suit" and "Rank"
-   // populate a List<card>array with all 52 cards
-   // print the size of the deck to the console
-        // sample output: 52 cards in the deck.
-
-    final int size = 52;
-    Card[] deckOfCards = new Card[size];
-
-
-    public DeckOfCards(){
-
-        int count=0;
-
-        String[] suit = { "Diamonds", "Clubs", "Hearts", "Spades" };
-        String[] rank = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
-
-        for (String s:suit){
-            for (String r:rank){
-
-                Card card = new Card(s, r);
-                this.deckOfCards[count] = card;
-                count++;
             }
+
+
         }
 
-    }
-    
-    // shuffle the deck method()
-    // should be random every time the program is run
-    public void ShuffleCards(){
-        Random rand = new Random();
-        int j;
-        for(int i=0; i<size; i++){
-            j = rand.nextInt(52);
-            Card temp = deckOfCards[i];
-            deckOfCards[i]=deckOfCards[j];
-            deckOfCards[j]= temp;
-        }
-    }
-
-    // print the deck of cards to the console with a Sorted by Rank method()
-    // the order should be by rank then suit eg. (printing new lines for all):
-        // FOUR of SPADES
-        // NINE of HEARTS
-        // FIVE of DIAMONDS
-
-
-
-
-    // sort the deck of cards with a Sorted by Rank method()
-    // where TWO is the smallest, and ACE is the largest eg:
+        // sort the deck of cards
+        // where TWO is the smallest, and ACE is the largest eg:
         // TWO of DIAMONDS
         // TWO OF HEARTS
-        // ..
+        // 
         // THREE of CLUBS
 
+        rank.sort(arr);
+        System.out.println(rank.toString(arr));
 
 
+        /* I could make a class and set public static final ints for each card
+        eg 
+	    public static final int TWO = 2;
+	    public static final int THREE = 3;
+	    public static final int FOUR = 4;
+        etc up to 13
+        */ 
+       
 
-    // sort the deck of cards by suit first then rank with a Sorted by Suit and Rank method()
-    // eg:
-    // TWO of CLUBS
-    // THREE of CLUBS
-    // ..
-    // TWO of DIAMONDS
-    // THREE of DIAMONDS
+        // sort the deck of cards by suit first then rank with a Sorted by Suit and Rank method()
+        // eg:
+        // TWO of CLUBS
+        // THREE of CLUBS
+        // ..
+        // TWO of DIAMONDS
+     // THREE of DIAMONDS
+    
+        suit.sort(arr);
+        rank.sort(arr);
+        system.out.println(suit.toString(arr).rank.toString(arr));
+    }
 
 
-
+}
+// I could do this a bunch of ways, and in fact, I would like to make a game out of this implementing a deal method to players 
+// of randomly drawn cards!
--- module-1/04_Loops_and_Arrays/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ module-1/04_Loops_and_Arrays/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -4,9 +4,44 @@ public class Tutorial {
 
     public static void main(String[] args) {
 
-        // write your code here
+        int i;
 
+        for (i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
 
+        for (i =10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        int[] forecastTemperatures = new int[5];
+        forecastTemperatures[0] = 72;
+        forecastTemperatures[0] = 78;
+        forecastTemperatures[0] = 58;
+        forecastTemperatures[0] = 79;
+        forecastTemperatures[0] = 74;
+
+        forecastTemperatures[2] += 10;
+
+        for (i = 0; i < forecastTemperatures.length; i++) {
+            System.out.println(forecastTemperatures[i]);
+        }
+
+        int highestTemperatureValue = forecastTemperatures[0];
+        int highestTemperatureIndex = 0;
+
+        for(int j = 1; j < forecastTemperatures.length; j++);
+
+        for (int j = 1; j < forecastTemperatures.length; j++) {
+            if (forecastTemperatures[j] > highestTemperatureValue) {
+                highestTemperatureValue = forecastTemperatures[j];
+                highestTemperatureIndex = j;
+            }
+        }
+
+        System.out.println("The highest temperature is " + highestTemperatureValue);
+
+        System.out.println("The highest temperature is in " + (highestTemperatureIndex + 1) + " days");
 
 
 
